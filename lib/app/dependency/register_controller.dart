d5dc26320819ba14257954a7e26139403b1544cb
Dashboard-toro <smuara20@gmail.com>
2026-08-29 11:37:02 +0700

    feat: register finance usecases and controller

--- lib/app/dependency/register_controller.dart
+++ lib/app/dependency/register_controller.dart
@@ -1,1 +1,19 @@
-void registerController() {}
+import '../../features/finance/application/usecases/delete_transaction_usecase.dart';
+import '../../features/finance/application/usecases/get_transaction_by_id_usecase.dart';
+import '../../features/finance/application/usecases/get_transactions_usecase.dart';
+import '../../features/finance/application/usecases/save_transaction_usecase.dart';
+import '../../features/finance/application/usecases/update_transaction_usecase.dart';
+import '../../features/finance/presentation/controllers/finance_controller.dart';
+import 'service_locator.dart';
+
+void registerController() {
+  sl.registerFactory<FinanceController>(
+    () => FinanceController(
+      getTransactionsUseCase: sl<GetTransactionsUseCase>(),
+      getTransactionByIdUseCase: sl<GetTransactionByIdUseCase>(),
+      saveTransactionUseCase: sl<SaveTransactionUseCase>(),
+      updateTransactionUseCase: sl<UpdateTransactionUseCase>(),
+      deleteTransactionUseCase: sl<DeleteTransactionUseCase>(),
+    ),
+  );
+}
--- lib/app/dependency/register_usecase.dart
+++ lib/app/dependency/register_usecase.dart
@@ -1,1 +1,29 @@
-void registerUsecase() {}
+import '../../features/finance/application/usecases/delete_transaction_usecase.dart';
+import '../../features/finance/application/usecases/get_transaction_by_id_usecase.dart';
+import '../../features/finance/application/usecases/get_transactions_usecase.dart';
+import '../../features/finance/application/usecases/save_transaction_usecase.dart';
+import '../../features/finance/application/usecases/update_transaction_usecase.dart';
+import '../../features/finance/domain/repository/finance_repository.dart';
+import 'service_locator.dart';
+
+void registerUsecase() {
+  sl.registerLazySingleton<GetTransactionsUseCase>(
+    () => GetTransactionsUseCase(repository: sl<FinanceRepository>()),
+  );
+
+  sl.registerLazySingleton<GetTransactionByIdUseCase>(
+    () => GetTransactionByIdUseCase(repository: sl<FinanceRepository>()),
+  );
+
+  sl.registerLazySingleton<SaveTransactionUseCase>(
+    () => SaveTransactionUseCase(repository: sl<FinanceRepository>()),
+  );
+
+  sl.registerLazySingleton<UpdateTransactionUseCase>(
+    () => UpdateTransactionUseCase(repository: sl<FinanceRepository>()),
+  );
+
+  sl.registerLazySingleton<DeleteTransactionUseCase>(
+    () => DeleteTransactionUseCase(repository: sl<FinanceRepository>()),
+  );
+}
