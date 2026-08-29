5a61dfee8cb70a96926d70fa1b21c44fcf7362dd
Dashboard-toro <smuara20@gmail.com>
2026-08-29 12:36:09 +0700

    29 agustus 11 siang

--- lib/app/dependency/register_mapper.dart
+++ lib/app/dependency/register_mapper.dart
@@ -1,11 +1,18 @@
-import 'package:get_it/get_it.dart';
-
-import '../../features/customer/data/mapper/customer_mapper.dart';
-
-final getIt = GetIt.instance;
-
-void registerMappers() {
-  getIt.registerLazySingleton<CustomerMapper>(
-        () => const CustomerMapper(),
-  );
-}
+Set-Content -Encoding UTF8 lib\app\dependency\register_mapper.dart @(
+"import 'package:get_it/get_it.dart';"
+""
+"import '../../features/customer/data/mapper/customer_mapper.dart';"
+"import '../../features/finance/data/mapper/transaction_mapper.dart';"
+""
+"final getIt = GetIt.instance;"
+""
+"void registerMappers() {"
+"  getIt.registerLazySingleton<CustomerMapper>("
+"    () => const CustomerMapper(),"
+"  );"
+""
+"  getIt.registerLazySingleton<TransactionMapper>("
+"    () => const TransactionMapper(),"
+"  );"
+"}"
+)
